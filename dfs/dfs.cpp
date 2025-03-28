181b359c320fd036f61cb04a7f67768eb86fc2ba
robertobbaik <robertobbaik@gmail.com>
2025-03-28 12:38:40 +0900

    0328

--- dfs/dfs.cpp
+++ dfs/dfs.cpp
@@ -1,11 +1,50 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
+void dfs(int node, vector<vector<int>> &graph, vector<bool> &visited, vector<int> &visitOrder)
+{
+    visited[node] = true;
+    visitOrder.push_back(node); // 방문 순서 저장
+
+    for (int i = 0; i < graph.size(); ++i)
+    {
+        if (graph[node][i] == 1 && !visited[i])
+        {
+            dfs(i, graph, visited, visitOrder);
+        }
+    }
+}
+
+vector<int> solution(int n, vector<vector<int>> computers, int start)
+{
+    vector<bool> visited(n, false);
+    vector<int> visitOrder;
+
+    dfs(start, computers, visited, visitOrder);
+
+    return visitOrder;
+}
 
 int main(void)
 {
     cout << "Hello, DFS" << endl;
+
+    int n = 5;
+    vector<vector<int>> computers = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 0, 1, 1},
+        {1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0}};
+    int start = 0;
+
+    vector<int> result = solution(n, computers, start);
+    for (int node : result)
+    {
+        cout << node << " ";
+    }
 
     return 0;
 }
---
+++ dfs/networkcount.cpp
@@ -0,0 +1,13 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello Network Count" << endl;
+
+    return 0;
+}
