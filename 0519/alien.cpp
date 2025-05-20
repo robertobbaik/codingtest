50a1fefbcda08ce197cc61d264c3c05f81736c65
백종규 <robertobbaik@gmail.com>
2025-05-20 14:02:44 +0900

    0520

---
+++ 0519/alien.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    freopen("alien.txt", "r", stdin);
+
+    int N, P;
+
+    cin >> N >> P;
+
+    for(int i = 0; i < N; i++)
+    {
+        
+    }
+
+
+    return 0;
+}
