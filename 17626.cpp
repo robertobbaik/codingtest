b486c0ae0362b415a1e995a6bd97a64b6662f46e
robertobbaik <robertobbaik@gmail.com>
2025-08-10 15:48:59 +0900

    0809

---
+++ 17626.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector<int> dp(n + 1, 4);
+	dp[0] = 0;
+
+	for (int i = 1; i < n + 1; i++)
+	{
+		for (int j = 1; j * j <= i; j++)
+		{
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+
+	cout << dp[n] << endl;
+
+	return 0;
+}
