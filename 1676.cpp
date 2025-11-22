2720d038bc347e0ae3568d5c21f1317f4da20a41
백종규 <robertobbaik@gmail.com>
2025-11-22 10:57:08 +0900

    1122

--- 1676.cpp
+++ 1676.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-	// 코드 작성
+	int N;
+	cin >> N;
+	
+	int count = 0;
+	for (int i = 5; i <= N; i *= 5)
+	{
+		count += N / i;
+	}
+	
+	cout << count << endl;
 	return 0;
 }
